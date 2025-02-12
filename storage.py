9f3d3764e8389fddaeb9950806051a5d2e024a27
gdread3 <gdread3@gmail.com>
2025-02-11 21:35:32 -0700

    added storage.py

---
+++ storage.py
@@ -0,0 +1,9 @@
+import json
+from datetime import datetime
+from expense import Expense
+
+def save_expenses(expenses, filename="expenses.json"):
+    pass
+
+def load_expenses(filename="expenses.json"):
+    pass
